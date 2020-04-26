df136b7c64632dd52e695fd6613d9b776761c865
Ester <esty8482@gmail.com>
2020-04-26 13:36:28 +0300

    finish api get for every option

--- src/api.py
+++ src/api.py
@@ -17,8 +17,12 @@ def add_message():
 #GET
 @app.route("/GetMessage", methods=['GET'])
 def get_message_application():
-    applicationId=request.args.get('applicationId',type = int)
-    list_messages = [element for element in Message.listMessage if element["application_id"]==applicationId]
+    applicationId=request.args.get('applicationId', default = None ,type = int)
+    sessionId=request.args.get('sessionId', default = None , type = str)
+    messageId = request.args.get('messageId', default=None, type=str)
+    list_messages = [element for element in Message.listMessage if element["application_id"]==applicationId or element["session_id"]==sessionId or element["message_id"]==messageId ]
+    if messageId != None and list_messages.__len__()==1:
+        return jsonify(list_messages[0])
     return jsonify(list_messages)
 
 
