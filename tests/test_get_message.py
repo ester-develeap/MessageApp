b6ff5a82d44eca8b488cfa892ad0684ff7c79f47
Ester <esty8482@gmail.com>
2020-04-29 00:48:48 +0300

    finish get test

---
+++ tests/test_get_message.py
@@ -0,0 +1,102 @@
+import json
+
+from flask import request
+
+from src.api import app
+
+def test_get_by_application():
+    client = app.test_client()
+    url = '/AddMessage'
+    data = {'application_id': 1,
+            'message_id': "aaa",
+            'participants': ["avi", "moshe"],
+            'session_id': "www",
+            'content': "like"}
+    response = client.post(url, data=json.dumps(data))
+    data = {'application_id': 1,
+            'message_id': "ddd",
+            'participants': ["avi", "moshe"],
+            'session_id': "xxx",
+            'content': "wow"}
+    response = client.post(url, data=json.dumps(data))
+
+    url = '/GetMessage?applicationId=1'
+    response = client.get(url)
+    data=response.get_data()
+    data = json.loads(data.decode())
+    assert data.__len__() == 2
+    assert response.status_code == 200
+
+    url = '/GetMessage?applicationId="sss"'
+    response = client.get(url)
+    data=response.get_data()
+    data = json.loads(data.decode())
+    assert data.__len__() == 0
+    assert response.status_code == 200
+
+    url = '/GetMessage?applicationId=15'
+    response = client.get(url)
+    data=response.get_data()
+    data = json.loads(data.decode())
+    assert data.__len__() == 0
+    assert response.status_code == 200
+
+def test_get_by_session():
+    client = app.test_client()
+    url = '/AddMessage'
+    data = {'application_id': 1,
+            'message_id': "aaa",
+            'participants': ["avi", "moshe"],
+            'session_id': "www",
+            'content': "like"}
+    response = client.post(url, data=json.dumps(data))
+    data = {'application_id': 2,
+            'message_id': "bbb",
+            'participants': ["avi", "moshe"],
+            'session_id': "www",
+            'content': "ooops"}
+    response = client.post(url, data=json.dumps(data))
+
+    url = '/GetMessage?sessionId=www'
+    response = client.get(url)
+    data=response.get_data()
+    data = json.loads(data.decode())
+    assert data.__len__() == 2
+    assert response.status_code == 200
+
+    url = '/GetMessage?sessionId=3'
+    response = client.get(url)
+    data=response.get_data()
+    data = json.loads(data.decode())
+    assert data.__len__() == 0
+    assert response.status_code == 200
+
+def test_get_by_messagen():
+    client = app.test_client()
+    url = '/AddMessage'
+    data = {'application_id': 1,
+            'message_id': "aaa",
+            'participants': ["avi", "moshe"],
+            'session_id': "www",
+            'content': "like"}
+    response = client.post(url, data=json.dumps(data))
+    data = {'application_id': 2,
+            'message_id': "bbb",
+            'participants': ["avi", "moshe"],
+            'session_id': "www",
+            'content': "ooops"}
+    response = client.post(url, data=json.dumps(data))
+
+    url = '/GetMessage?messageId=aaa'
+    response = client.get(url)
+    data = response.get_data()
+    data = json.loads(data.decode())
+    assert data["message_id"] == "aaa"
+    assert response.status_code == 200
+
+    url = '/GetMessage?messageId=5'
+    response = client.get(url)
+    data = response.get_data()
+    data = json.loads(data.decode())
+    assert data == []
+    assert response.status_code == 200
